8dc05369559abc2fa85a7ee2a5942b30c7ee2935
Ayush <avinashrajayush@gmail.com>
2024-09-11 16:07:03 +0530

    dist build error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     historyApiFallback: true
   },
   build: {
-    outDir: 'build',  // Ensure this matches the directory name used in Render
+    outDir: 'dist',  // Ensure this matches the directory name used in Render
   },
   plugins: [react()],
 })

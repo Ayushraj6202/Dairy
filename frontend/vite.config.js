a1b377006c6323a9031efc74ae9b880bea2f18e7
Ayush <avinashrajayush@gmail.com>
2024-09-11 19:45:52 +0530

    netlify toml added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true,  // Ensures client-side routing works during development
-  },
+  // build: {
+  //   outDir: 'dist',
+  // },
+  // server: {
+  //   historyApiFallback: true,  // Ensures client-side routing works during development
+  // },
 });
